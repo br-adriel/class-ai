f9a154208120349f4e815aa1593da0ded39eb135
Adriel <adriel.fsantos@outlook.com>
2023-10-29 23:53:04 -0300

    Fix none type error

--- search/utils/path.py
+++ search/utils/path.py
@@ -12,5 +12,5 @@ def path_dict_to_list(path: dict[str, Union[int, None]]) -> list[int]:
     while not next_key == list_path[-1]:
         list_path.append(next_key)
         next_key = path.get(list_path[-1])
-    list_path = [int(i) for i in list_path]
+    list_path = [int(i) for i in list_path if i is not None]
     return list_path
